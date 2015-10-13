241ca1d0bffa8be1d26a2faad138d4afc3325127
Thomas Pike <thwpike@crossroads.org.hk>
2015-10-13 19:24:27 +0800

    Initial work on crf-motd

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'crf-motd'
+maintainer       'Crossroads Foundation Ltd'
+maintainer_email 'itdept@crossroads.org.hk'
+license          'Apache 2.0'
+description      'Installs/Configures crf-motd'
+long_description 'Installs/Configures crf-motd'
+version          '0.1.0'
+
+depends 'motd'
