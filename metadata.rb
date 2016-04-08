8135ea4c18a22e784b39d528564434471429f014
Thomas Pike <thwpike@crossroads.org.hk>
2016-04-08 22:07:32 +0800

    Making foodcritic happy

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures crf-motd'
 long_description 'Installs/Configures crf-motd'
 version          '0.1.0'
+issues_url       'https://github.com/crossroads/crf-motd/issues'
+source_url       'https://github.com/crossroads/crf-motd'
 
 depends 'motd'
